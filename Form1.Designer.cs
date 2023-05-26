77e674fc5e72cadc2ca66e30fe5d3cbd1e0883e4
KaitoIto <kaito.itou08@gmail.com>
2023-05-27 02:17:24 +0900

    任意のキーで右画面の画像ファイル保存ができるように実装

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,29 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtSelectedFile = new System.Windows.Forms.Label();
+            this.btnOpenDialog = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnShot = new System.Windows.Forms.Button();
+            this.txtPicName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // txtSelectedFile
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(396, 367);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "test";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.txtSelectedFile.AutoSize = true;
+            this.txtSelectedFile.Location = new System.Drawing.Point(311, 660);
+            this.txtSelectedFile.Name = "txtSelectedFile";
+            this.txtSelectedFile.Size = new System.Drawing.Size(0, 12);
+            this.txtSelectedFile.TabIndex = 0;
+            this.txtSelectedFile.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnOpenDialog
+            // 
+            this.btnOpenDialog.Location = new System.Drawing.Point(266, 601);
+            this.btnOpenDialog.Name = "btnOpenDialog";
+            this.btnOpenDialog.Size = new System.Drawing.Size(85, 23);
+            this.btnOpenDialog.TabIndex = 1;
+            this.btnOpenDialog.Text = "フォルダを選択";
+            this.btnOpenDialog.UseVisualStyleBackColor = true;
+            this.btnOpenDialog.Click += new System.EventHandler(this.btnOpenDialog_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(292, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(720, 480);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnShot
+            // 
+            this.btnShot.Location = new System.Drawing.Point(377, 553);
+            this.btnShot.Name = "btnShot";
+            this.btnShot.Size = new System.Drawing.Size(75, 23);
+            this.btnShot.TabIndex = 3;
+            this.btnShot.Text = "撮影";
+            this.btnShot.UseVisualStyleBackColor = true;
+            this.btnShot.Click += new System.EventHandler(this.btnShot_Click);
+            // 
+            // txtPicName
+            // 
+            this.txtPicName.Location = new System.Drawing.Point(583, 519);
+            this.txtPicName.Name = "txtPicName";
+            this.txtPicName.Size = new System.Drawing.Size(100, 19);
+            this.txtPicName.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPicName);
+            this.Controls.Add(this.btnShot);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOpenDialog);
+            this.Controls.Add(this.txtSelectedFile);
+            this.KeyPreview = true;
             this.MaximumSize = new System.Drawing.Size(1280, 720);
             this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,7 +105,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtSelectedFile;
+        private System.Windows.Forms.Button btnOpenDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnShot;
+        private System.Windows.Forms.TextBox txtPicName;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
 
 namespace SingleScreenShot
 {
     public partial class Form1 : Form
     {
+        int i = 0;
         public Form1()
         {
             InitializeComponent();
@@ -21,5 +26,52 @@ namespace SingleScreenShot
         {
 
         }
+
+        private void btnOpenDialog_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSelectedFile.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnShot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void d(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Constants.shotKey)
+            {
+                Rectangle rectangle = new Rectangle(1920, 0, 1920, 1080);
+                Bitmap bitmap = new Bitmap(rectangle.Width, rectangle.Height);
+                Graphics graphics = Graphics.FromImage(bitmap);
+                graphics.CopyFromScreen(new Point(rectangle.X, rectangle.Y), new Point(0, 0), bitmap.Size);
+               
+                graphics.Dispose();
+                
+                if (folderBrowserDialog1.SelectedPath.Length < 1)
+                    MessageBox.Show("保存先を入力してください");
+                else
+                {
+                    pictureBox1.Image = bitmap;
+                    MessageBox.Show(folderBrowserDialog1.SelectedPath);
+                    txtSelectedFile.Text = folderBrowserDialog1.SelectedPath;
+                    bitmap.Save(@folderBrowserDialog1.SelectedPath + "\\" + txtPicName.Text + i,ImageFormat.Jpeg);
+                    i++;
+                }
+            }
+        }
+    }
+    static class Constants
+    {
+        public const Keys shotKey = Keys.F1;
     }
 }
